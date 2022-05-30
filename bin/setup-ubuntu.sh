cd26108bd10fd3096c0fccd3684fab4ab187955b
sergiu128 <sergiu.marin@talostrading.com>
2022-05-30 17:31:08 +0200

    Doc ubuntu golang setup

--- bin/setup-ubuntu.sh
+++ bin/setup-ubuntu.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+# this installs stuff needed to run and debug Go code easily on Ubuntu
+# after this script runs, type vim and then :PlugUpdate
+# then go mod tidy your go repo
+# open any file in the repo
+# g-d will take you to the function definition
+# shift-k will give you the docs of the function
+# go to the left tab with shift-o
+# go to the right tab with shift-p
+
+cd ~/
+
+git clone https://github.com/sergiu128/dotfiles.git
+mkdir -p .config
+mkdir -p .config/nvim
+cp dotfiles/.config/nvim/init.vim .config/nvim/init.vim
+cd .config/nvim/
+ln -s ~/.vim/autoload .
+
 sudo snap install node --classic
 sudo snap install nvim --classic
 sudo snap install go --classic
